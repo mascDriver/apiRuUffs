6d9f3f2f464cca47e97218acdec09ba2a62d5887
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-05-04 17:33:14 -0300

    Campus Erechim está no ar.

--- main.py
+++ main.py
@@ -19,9 +19,6 @@ async def home():
 
 @app.get("/campus/{campus}")
 async def ver_cardapio_campus(campus: str, response: Response):
-    if campus == 'erechim':
-        response.status_code = status.HTTP_302_FOUND
-        return {"message": f"Campus {campus} está em desenvolvimento."}
     html = get_cardapio(campus)
     if not html:
         response.status_code = status.HTTP_404_NOT_FOUND
