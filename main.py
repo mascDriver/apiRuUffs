e24d4f49881a35ff586d7c64ea0f05b8d2869d15
Diogo Baltazar <ti3@ballke.com.br>
2022-05-23 17:44:14 -0300

    adicionando redis cache para melhora na consulta, cache expirando em 86400

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI, Response, status
+import aioredis
+from fastapi import FastAPI
+from fastapi import status
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from starlette.responses import Response
+
 from webscraping.get_data import get_cardapio, prepare_data, get_cardapio_dia
 
 app = FastAPI(
@@ -12,33 +19,44 @@ app = FastAPI(
 )
 
 
+@app.on_event("startup")
+async def startup():
+    redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
 @app.get("/")
+@cache(expire=86400)
 async def home():
-    return {"message": f"Bem vindo a api ru uffs, acesse {app.docs_url} para mais informações"}
+    return dict(message=f"Bem vindo a api ru uffs, acesse {app.docs_url} para mais informações")
 
 
 @app.get("/campus/{campus}")
+@cache(expire=86400)
 async def ver_cardapio_campus(campus: str, response: Response):
     if campus == 'erechim':
         response.status_code = status.HTTP_302_FOUND
-        return {"message": f"Campus {campus} está em desenvolvimento."}
+        return dict(message=f"Campus {campus} está em desenvolvimento.")
     bs = get_cardapio(campus)
     if not bs:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"message": f"Campus {campus} não encontrado."}
+        return dict(message=f"Campus {campus} não encontrado.")
     cardapio = prepare_data(bs)
     response.status_code = status.HTTP_200_OK
     return {'cardapios': cardapio}
 
+
 @app.get("/campus/{campus}/dia/{dia}")
+@cache(expire=86400)
 async def ver_cardapio_campus_dia(campus: str, dia: int, response: Response):
     bs = get_cardapio(campus)
     if not bs:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"message": f"Campus '{campus}' não encontrado."}
+        return dict(message=f"Campus '{campus}' não encontrado.")
+
     cardapios = prepare_data(bs)
-    if dia not in range(0,5):
+    if dia not in range(0, 5):
         response.status_code = status.HTTP_400_BAD_REQUEST
-        return {"message": f"Informe um dia entre 0 - 4."}
+        return dict(message=f"Informe um dia entre 0 - 4.")
     cardapio = get_cardapio_dia(dia, cardapios)
-    return {'cardapios': cardapio}
+    return dict(cardapios=cardapio)
